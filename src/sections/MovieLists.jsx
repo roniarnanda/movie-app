a7836040bdf265190067422722b46b3f1377de23
Roni <roniarnandaa@gmail.com>
2025-06-27 01:02:56 +0700

    Penyesuaian akhir

--- src/sections/MovieLists.jsx
+++ src/sections/MovieLists.jsx
@@ -3,6 +3,14 @@ import MovieCard from '../components/MovieCard';
 import Spinner from '../components/Spinner';
 
 const MovieLists = ({ movieList, isLoading, errorMessage }) => {
+  // ID film yang ingin dikecualikan
+  const excludedMovieId = 1395724;
+
+  // Filter movieList untuk mengecualikan film dengan ID tertentu
+  const filteredMovies = movieList.filter(
+    (movie) => movie.id !== excludedMovieId
+  );
+
   return (
     <section className="all-movies">
       <h2 className="mt-[40px]">All Movies</h2>
@@ -12,7 +20,7 @@ const MovieLists = ({ movieList, isLoading, errorMessage }) => {
         <p className="text-red-500">{errorMessage}</p>
       ) : (
         <ul>
-          {movieList.map((movie) => (
+          {filteredMovies.map((movie) => (
             <MovieCard key={movie.id} movie={movie} />
           ))}
         </ul>
