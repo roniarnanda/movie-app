ecdb5fb95cb64cdbc63df2455c6192b585207f43
Roni <roniarnandaa@gmail.com>
2025-06-26 14:13:51 +0700

    Menambahkan fitur pencarian

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -4,8 +4,6 @@ const Search = ({ searchTerm, setSearchTerm }) => {
   return (
     <div className="search">
       <div>
-        <img src="search.svg" alt="search" />
-
         <input
           type="text"
           placeholder="Search Movie"
